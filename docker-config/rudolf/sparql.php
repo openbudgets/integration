a6017902cb3c6c7f874e730e9131e2a602338062
Maik Lukasch <mlukasch@gmail.com>
2016-06-10 17:33:58 +0200

    rudolf added

---
+++ docker-config/rudolf/sparql.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: larjohns
+ * Date: 26/02/2016
+ * Time: 20:12:49
+ */
+
+return [
+    "prefixes"=>[
+        'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
+        'qb' => 'http://purl.org/linked-data/cube#',
+        'skos'=>'http://www.w3.org/2004/02/skos/core#',
+        'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+        'xro' => 'http://purl.org/xro/ns#'
+    ],
+    "endpoint" =>'http://localhost:8890/sparql'
+
+];
