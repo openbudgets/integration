7a4d4b7b61a687e37bb9d80e8ea3cd2422fc4bf2
mlukasch <mlukasch@gmail.com>
2016-06-17 15:08:03 +0200

    fix

--- docker-config/fuseki/start.sh
+++ docker-config/fuseki/start.sh
@@ -1,1 +1,2 @@
-cd $FUSEKI_HOME && sh ./fuseki-server --update --config=/config.ttl
+#cd $FUSEKI_HOME && sh ./fuseki-server --update --config=/config.ttl
+cd $FUSEKI_HOME && sh ./fuseki-server --update fuseki-server --loc=/data /fuseki
--- fusekiTestCommands.sh
+++ fusekiTestCommands.sh
@@ -2,13 +2,13 @@
 # Needs apache-fuseki/bin folder in the PATH:
 
 # Load some RDF data into the default graph of the server:
-s-put http://localhost:33030/ontology default test.ttl
-s-put http://localhost:33030/resource default test.ttl
-s-put http://eis-openbudgets.iais.fraunhofer.de/fuseki/data default test.ttl
+s-put http://localhost:33030/ontology/data default test.ttl
+s-put http://localhost:43030/resource/data default test.ttl
+
 
 # Get it back:
-s-get http://localhost:33030/fuseki/data default
-s-get http://localhost:33030/fuseki/data default
+s-get http://localhost:33030/ontology/data default
+s-get http://localhost:33030/resource/data default
 
 # Query it with SPARQL using the .../query endpoint.
 s-query --service http://localhost:33030/fuseki/query 'SELECT * {?s ?p ?o}'
@@ -19,8 +19,10 @@ http://localhost:33030/fuseki/query?query=DESCRIBE%20%3Chttp%3A%2F%2Flocalhost%3
 curl http://localhost:33030/fuseki/query?query=DESCRIBE%20%3Chttp%3A%2F%2Flocalhost%3A33030%2Ffuseki%2Fdata%3E
 
 # How it should be with url rewriting:
-curl -L http://localhost/fuseki/data
-curl -L http://eis-openbudgets.iais.fraunhofer.de/fuseki/data
+curl -L http://localhost/resources/data
+curl -L http://localhost/ontology/data
+curl -L http://eis-openbudgets.iais.fraunhofer.de/resources/data
+curl -L http://eis-openbudgets.iais.fraunhofer.de/ontology/data
 
 # Update it with SPARQL using the .../update endpoint.
 s-update --service http://localhost:33030/fuseki/update 'CLEAR DEFAULT'
