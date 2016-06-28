da7d4c65c9703ba1ab3c14e5df69dab3d176a171
mlukasch <mlukasch@gmail.com>
2016-06-28 19:51:28 +0200

    added docu

---
+++ testfiles/sendToPipelineExternal.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PARAM_FILE=$PWD/datapackage.jsonld
+PARAM_LP_DOMAIN=http://eis-openbudgets.iais.fraunhofer.de
+
+curl -i -X POST -H "Content-Type: multipart/form-data" -F "input=@$PARAM_FILE" "$PARAM_LP_DOMAIN/linkedpipes/execute/fdp2rdf"
