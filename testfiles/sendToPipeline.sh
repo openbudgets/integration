75e8e9801e8beb64621c352b21eb57d7e8f9ff84
mlukasch <mlukasch@gmail.com>
2016-06-28 18:10:54 +0200

    added some docu

--- testfiles/sendToPipeline.sh
+++ testfiles/sendToPipeline.sh
@@ -1,18 +1,25 @@
-#Sending a POST to trigger the pipeline:
-#URL =
-#http://[your LinkedPipes server]/resources/executions?pipeline=http://[your LinkedPipes server]/resources/pipelines/created-[pipeline timestamp]
+# Script for Sending a POST to trigger the pipeline execution:
 
-INPUT_FILE=$PWD/datapackage.jsonld
-INPUT_PIPELINE=created-1467031005380
-INPUT_LP_DOMAIN=http://localhost:8080
-echo "INPUT_FILE=$INPUT_FILE"
-echo "INPUT_PIPELINE=$INPUT_PIPELINE"
-echo "INPUT_LP_DOMAIN=$INPUT_LP_DOMAIN"
-echo "curl -i -X POST -H \"Content-Type: multipart/form-data\" -F \"input=@$INPUT_FILE\" \"$INPUT_LP_DOMAIN/resources/executions?pipeline=$INPUT_LP_DOMAIN/resources/pipelines/$INPUT_PIPELINE\""
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "input=@$INPUT_FILE" "$INPUT_LP_DOMAIN/resources/executions?pipeline=$INPUT_LP_DOMAIN/resources/pipelines/$INPUT_PIPELINE"
+# Tutorial by Marek: https://github.com/openbudgets/pipeline-fragments/tree/master/FDPtoRDF
 
-# Output:
-# INPUT_FILE=/home/maik/obeu_infrastructure2/testfiles/datapackage.jsonld
-# INPUT_PIPELINE=created-1467031005380
-# INPUT_LP_DOMAIN=http://localhost:8080
+# LinkedPipes-URL for triggering the pipeline execution =
+# http://[your LinkedPipes server]/resources/executions?pipeline=http://[your LinkedPipes server]/resources/pipelines/created-[pipeline timestamp]
+
+# Parameters:
+PARAM_FILE=$PWD/datapackage.jsonld
+PARAM_PIPELINE=created-1467031005380
+PARAM_LP_DOMAIN=http://localhost:8080
+
+echo "PARAM_FILE=$INPUT_FILE"
+echo "PARAM_PIPELINE=$INPUT_PIPELINE"
+echo "PARAM_LP_DOMAIN=$INPUT_LP_DOMAIN"
+echo "curl -i -X POST -H \"Content-Type: multipart/form-data\" -F \"input=@$PARAM_FILE\" \"$PARAM_LP_DOMAIN/resources/executions?pipeline=$PARAM_LP_DOMAIN/resources/pipelines/$PARAM_PIPELINE\""
+
+# Sending POST to LinkedPipes:
+curl -i -X POST -H "Content-Type: multipart/form-data" -F "input=@$PARAM_FILE" "$PARAM_LP_DOMAIN/resources/executions?pipeline=$PARAM_LP_DOMAIN/resources/pipelines/$PARAM_PIPELINE"
+
+# echo output:
+# PARAM_FILE=/home/maik/obeu_infrastructure2/testfiles/datapackage.jsonld
+# PARAM_PIPELINE=created-1467031005380
+# PARAM_LP_DOMAIN=http://localhost:8080
 # curl -i -X POST -H "Content-Type: multipart/form-data" -F "input=@/home/maik/obeu_infrastructure2/testfiles/datapackage.jsonld" "http://localhost:8080/resources/executions?pipeline=
