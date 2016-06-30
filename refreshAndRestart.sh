66c41900b90ca0ce53a4b8ac42b13cb6b212f0d7
mlukasch <mlukasch@gmail.com>
2016-06-30 09:45:50 +0200

    fix

--- refreshAndRestart.sh
+++ refreshAndRestart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git pull origin
 cd ./docker-config
-docker-compose stop && docker-compose rm -f
+docker-compose stop && docker-compose rm -f && docker-compose build
 sh ../removeLocks.sh
 docker-compose up -d
