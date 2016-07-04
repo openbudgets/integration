f4c8f39af30683ef0fd16bd57e86d86db7373c12
mlukasch <mlukasch@gmail.com>
2016-07-04 16:04:40 +0200

    fix nginx.conf for rudolf

--- infos/importToProdFuseki.sh
+++ infos/importToProdFuseki.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 ls {*.rdf,*.ttl} | xargs -i s-put http://localhost:43030/fuseki/data default {}
+# Pattern: s-put http://localhost:43030/<dataset>/data <named_graph> <ttl-file>
--- infos/importToStagFuseki.sh
+++ infos/importToStagFuseki.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 ls {*.rdf,*.ttl} | xargs -i s-put http://localhost:33030/fuseki/data default $1
+# Pattern: s-put http://localhost:33030/<dataset>/data <named_graph> <ttl-file>
