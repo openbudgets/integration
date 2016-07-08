87a7d7a823d3c9bc6ce7e1f6c7c739fdc167cbfa
mlukasch <mlukasch@gmail.com>
2016-07-08 13:08:14 +0200

    script for ssl cert fix

---
+++ requestCertificateProduction.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+mkdir -p ./volumes/nginx/letsencrypt/www
+mkdir -p ./volumes/nginx/letsencrypt/lib
+mkdir -p ./volumes/nginx/letsencrypt/etc
+
+docker run -it --rm --name letsencrypt \
+    -p 1086:80 \
+    -v "$PWD/volumes/nginx/letsencrypt/etc:/etc/letsencrypt" \
+    -v "$PWD/volumes/nginx/letsencrypt/lib:/var/lib/letsencrypt" \
+    -v "$PWD/volumes/nginx/letsencrypt/www:/var/www" \
+    quay.io/letsencrypt/letsencrypt:latest \
+    certonly \
+    --authenticator webroot \
+    --webroot \
+    --webroot-path /var/www/eis-openbudgets.iais.fraunhofer.de \
+    --agree-tos \
+    --renew-by-default \
+    -d eis-openbudgets.iais.fraunhofer.de \
+    -m mlukasch@gmail.com \
+    --server https://acme-staging.api.letsencrypt.org/directory \
+
+docker kill --signal=HUP dockerconfig_nginx_1
