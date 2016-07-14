04db173b8b5944eb632e904f571f11aef5dcafad
mlukasch <mlukasch@gmail.com>
2016-07-14 18:12:21 +0200

    script added for syncing stagingdata to proddata

--- synchronizeTripleStores.sh
+++ synchronizeTripleStores.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Everything that is in A will be added to B:
 # A => B
-sudo rsync -az /home/path/folder1/ /home/path/folder2
-sudo rsync --dry-run -az /home/path/folder1/ /home/path/folder2
+STAGING_DATA=/srv2/obeu/integration/volumes/tripleStoreStaging/data/
+PRODUCTION_DATA=/srv2/obeu/integration/volumes/tripleStoreProduction/data/
+sudo rsync -az $STAGING_DATA $PRODUCTION_DATA
+sudo rsync --dry-run -az $STAGING_DATA $PRODUCTION_DATA
