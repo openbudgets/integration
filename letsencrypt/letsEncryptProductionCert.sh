d81595d80aea0b8ca4b72c1f279c6b17ed9b6416
mlukasch <mlukasch@gmail.com>
2016-07-18 15:45:40 +0200

    refactor

--- letsencrypt/letsEncryptProductionCert.sh
+++ letsencrypt/letsEncryptProductionCert.sh
@@ -3,16 +3,16 @@
 echo "If run for the first time, please disable https 443 in nginx.conf."
 echo "If the script has succefully run, enable https 443 in nginx.conf again."
 
-mkdir -p ./volumes/nginx/letsencrypt/www
-mkdir -p ./volumes/nginx/letsencrypt/lib
-mkdir -p ./volumes/nginx/letsencrypt/etc
+mkdir -p ../volumes/nginx/letsencrypt/www
+mkdir -p ../volumes/nginx/letsencrypt/lib
+mkdir -p ../volumes/nginx/letsencrypt/etc
 
-docker build -t letsencrypt_img ./docker-config/letsencrypt
+docker build -t letsencrypt_img .
 docker run -it --rm --name letsencrypt \
     -p 1086:80 \
-    -v "$PWD/volumes/nginx/letsencrypt/etc:/etc/letsencrypt" \
-    -v "$PWD/volumes/nginx/letsencrypt/lib:/var/lib/letsencrypt" \
-    -v "$PWD/volumes/nginx/letsencrypt/www:/var/www" \
+    -v "$PWD/../volumes/nginx/letsencrypt/etc:/etc/letsencrypt" \
+    -v "$PWD/../volumes/nginx/letsencrypt/lib:/var/lib/letsencrypt" \
+    -v "$PWD/../volumes/nginx/letsencrypt/www:/var/www" \
     letsencrypt_img \
     certonly \
     --authenticator webroot \
