6bfec166ecbe5f665fea506051b93b71c2e15a16
mlukasch <mlukasch@gmail.com>
2016-07-19 09:43:04 +0200

    reduced synching to daily

--- docker-config/fuseki/production/start.sh
+++ docker-config/fuseki/production/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Add cronjob:
-cp /cronjobs/syncTriplestores /etc/periodic/15min/syncTriplestores
+cp /cronjobs/syncTriplestores /etc/periodic/daily/syncTriplestores
 
 # Start Fuseki
 find . -name *.lock | xargs -i rm {}
