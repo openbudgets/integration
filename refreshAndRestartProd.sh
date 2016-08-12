a5ec74477b60ce4d896c3752897234ccd3e554ee
mlukasch <mlukasch@gmail.com>
2016-08-12 14:26:27 +0200

    fixed paths

--- refreshAndRestartProd.sh
+++ refreshAndRestartProd.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 git pull origin
+OBEU_WORKDIR=$PWD
 chmod -R a+rx ./docker-config
 chmod -R a+rx ./refreshAndRestartProd.sh
 chmod -R a+rx ./seeComposeLogs.sh
-cd ./docker-config
-docker-compose stop && docker-compose rm -f && docker-compose build
-sh ../removeFusekiLocks.sh
+cd ./docker-config && \
+    docker-compose stop && \
+    docker-compose rm -f && \
+    docker-compose build
+cd $OBEU_WORKDIR && \
+    /bin/sh removeFusekiLocks.sh
 docker-compose up -d
