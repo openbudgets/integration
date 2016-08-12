57580c7223aac9332f79ed4798eb73274d78299e
mlukasch <mlukasch@gmail.com>
2016-08-12 14:12:26 +0200

    user permissions

--- refreshAndRestartProd.sh
+++ refreshAndRestartProd.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 git pull origin
+chmod -R a+rx ./docker-config
 cd ./docker-config
 docker-compose stop && docker-compose rm -f && docker-compose build
 sh ../removeFusekiLocks.sh
