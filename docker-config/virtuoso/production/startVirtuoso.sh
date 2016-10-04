6c4955ca29c35b86503dd5817b2fbb7ecf3d36d8
mlukasch <maiklukasch@gmail.com>
2016-10-04 16:07:58 +0200

    cronjob fix

--- docker-config/virtuoso/production/startVirtuoso.sh
+++ docker-config/virtuoso/production/startVirtuoso.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # Install cronjob:
 #(crontab -l 2>/dev/null; echo "* * * * * /cronjobs/syncTriplestores") | crontab -
-(crontab -l 2>/dev/null; echo "@hourly /cronjobs/syncTriplestores") | crontab -
-
-service cron start
+(crontab -l 2>/dev/null; echo "0 * * * * /cronjobs/syncTriplestores") | crontab -
 
 # Start Virtuoso server:
 /bin/sh /virtuoso.sh
