66b0c9a3919ff898f3f6ba1811f15dfa90183648
mlukasch <maiklukasch@gmail.com>
2016-10-10 13:14:00 +0200

    bugfixing

--- docker-config/virtuoso/staging/importDump.sh
+++ docker-config/virtuoso/staging/importDump.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # 1st argument: DBA_PASSWORD
-echo "ld_dir_all('/dump', '*.ttl')" > tmp.isql
+echo "ld_dir_all('/dumps', '*.ttl')" > tmp.isql
 /usr/local/virtuoso-opensource/bin/isql-v 1111 dba $1 tmp.isql
 rm tmp.isql
