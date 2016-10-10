3bf1425309959ec10186488aadd6588348e9b3ce
mlukasch <maiklukasch@gmail.com>
2016-10-10 11:18:14 +0200

    updated rudolf sparql endpoint

--- docker-config/rudolf/config/sparql.php
+++ docker-config/rudolf/config/sparql.php
@@ -14,5 +14,5 @@ return [
         'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
         'xro' => 'http://purl.org/xro/ns#'
     ],
-    "endpoint" =>'http://triple_store_production:3030/fuseki/sparql'
+    "endpoint" =>'http://virtuoso_production:8890/sparql'
 ];
