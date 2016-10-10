668db5eb2b5e2ab9ece33e868f757df8fbb2f946
mlukasch <maiklukasch@gmail.com>
2016-10-10 15:23:58 +0200

    fix cronjob

--- docker-config/virtuoso/production/startVirtuoso.sh
+++ docker-config/virtuoso/production/startVirtuoso.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # Install cronjob:
 #(crontab -l 2>/dev/null; echo "* * * * * /cronjobs/syncTriplestores") | crontab -
-(crontab -l 2>/dev/null; echo "0 * * * * /cronjobs/syncTriplestores") | crontab -
+echo "0 * * * * /cronjobs/syncTriplestores" > mycron
+crontab mycron
+rm mycron
+# (crontab -l 2>/dev/null; echo "0 * * * * /cronjobs/syncTriplestores") | crontab -
 
 # Start Virtuoso server:
 /bin/sh /virtuoso.sh
--- docker-config/virtuoso/staging/importDump.sh
+++ docker-config/virtuoso/staging/importDump.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+# see: http://virtuoso.openlinksw.com/dataspace/doc/dav/wiki/Main/VirtBulkRDFLoader
 # 1st argument: DBA_PASSWORD
-echo "ld_dir_all('/dumps', '*.ttl')" > tmp.isql
+echo "DELETE FROM DB.DBA.load_list;" > tmp.isql
+echo "ld_dir_all('/dumps', '*.ttl',NULL);" >> tmp.isql
+echo "select * from DB.DBA.load_list;" >> tmp.isql
+echo "rdf_loader_run();" >> tmp.isql
 /usr/local/virtuoso-opensource/bin/isql-v 1111 dba $1 tmp.isql
 rm tmp.isql
