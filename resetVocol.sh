44b44225bbf357cd19ae9fb3abd17d926df1bd6c
mlukasch <maiklukasch@gmail.com>
2016-10-25 16:57:41 +0200

    added  reset script for vocol

---
+++ resetVocol.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker exec vocol_instance /bin/sh start.sh
--- startVocol.sh
+++ startVocol.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd ./docker-config/vocol/
 docker build -t vocol .
-docker run -p 8999:80 -d --restart="always" vocol
+docker run -p 8999:80 -d --name="vocol_instance" --restart="always" vocol
