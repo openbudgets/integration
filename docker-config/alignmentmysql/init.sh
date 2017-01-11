92b9cd041de393dd426c6f4f138c390d6d3f0785
mlukasch <maiklukasch@gmail.com>
2017-01-11 13:35:36 +0100

    fix

--- docker-config/alignmentmysql/init.sh
+++ docker-config/alignmentmysql/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "USE alignment;" > /docker-entrypoint-initdb.d/privileges.sql
-echo "GRANT ALL ON *.* to root@'%' IDENTIFIED BY '${ALIGNMENT_MYSQL_PW}';" >> /docker-entrypoint-initdb.d/privileges.sql
+echo "GRANT ALL ON *.* to root@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> /docker-entrypoint-initdb.d/privileges.sql
 echo "FLUSH PRIVILEGES;" >> /docker-entrypoint-initdb.d/privileges.sql
 
