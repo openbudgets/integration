8cb0dd53cfeb089eccb05d1e1a678a7a99d7dd0d
mlukasch <maiklukasch@gmail.com>
2017-01-17 10:57:00 +0100

    fix in letsencrypt script

--- letsencrypt/letsEncryptAddCronjob.sh
+++ letsencrypt/letsEncryptAddCronjob.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-#write out current crontab
+# Needs to be executed in the directory where the script is stored
+# write out current crontab
 crontab -l > mycron
-#echo new cron into cron file
-CMD="sh $PWD/letsEncryptRenewCert.sh"
+# echo new cron into cron file
+CMD="cd $PWD && sh ./letsEncryptRenewCert.sh"
 echo "@weekly $CMD" >> mycron
-#install new cron file
+# install new cron file
 crontab mycron
 rm mycron
