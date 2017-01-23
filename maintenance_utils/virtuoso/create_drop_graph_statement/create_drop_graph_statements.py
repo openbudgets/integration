6f8a40a44602b716ea4bcacf65efd971d9b1ebe0
mlukasch <maiklukasch@gmail.com>
2017-01-23 18:07:07 +0100

    added utility script for dropping graphs

---
+++ maintenance_utils/virtuoso/create_drop_graph_statement/create_drop_graph_statements.py
@@ -0,0 +1,23 @@
+import os
+import re
+
+# Parameter:
+# For example for Bonn:
+graph_search_pattern = "onn"
+
+
+graphs_file = "input_all_graph_names.txt"
+pattern = "http[\S]+\s"
+template = """CLEAR GRAPH <%s>;"""
+regex = re.compile(pattern)
+
+with(open(os.path.join(os.getcwd(), graphs_file))) as file:
+    lines = file.readlines()
+    statements = []
+    for line in lines:
+        results = regex.findall(line)
+        if results:
+            statements.append(results[0])
+    with(open(os.path.join(os.getcwd(), "output_drop_graph_statement.txt"), "w")) as write_file:
+        drops = [template % graph.strip() for graph in statements if graph_search_pattern in graph]
+        write_file.writelines([drop for drop in drops])
