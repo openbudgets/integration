a30b16c9fc010ba38318ed0bdeaada3cf58c8492
mlukasch <maiklukasch@gmail.com>
2017-01-25 16:26:50 +0100

    cleanup

--- install FdpToRdfComponent.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-URL_FDP2RDF_COMPONENT="https://github.com/opendatacz/lp-etl-components/raw/master/t-fdpToRdf/deploy/t-fdpToRdf.zip"
-wget $URL_FDP2RDF_COMPONENT
--- maintenance_utils/virtuoso/import/importDumpToVirtuoso.sh
+++ maintenance_utils/virtuoso/import/importDumpToVirtuoso.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# Please configure the Dump folder where the datasets are located for the import to Virtuoso
+# in the script /importDump.sh
+
 # Create graph-files for each ttl-file:
 echo "Creating graph-files for each ttl-file...."
 docker exec dockerconfig_triple_store_staging_1 /bin/sh /createGraphFiles.sh
@@ -6,6 +10,6 @@ echo "Created graph-files for each ttl-file"
 
 # Upload to Virtuoso:
 echo "Upload to Virtuoso..."
-. setEnv
+. ../../../setEnv
 docker exec dockerconfig_virtuoso_staging_1 /bin/sh /importDump.sh $VIRTUOSO_STAG_PW
 echo "Uploaded to Virtuoso."
