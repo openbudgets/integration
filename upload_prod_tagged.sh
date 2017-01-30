01b9328e2653b8b9075c4750ad0e8cfb1d14ea7e
mlukasch <maiklukasch@gmail.com>
2017-01-30 16:53:05 +0100

    fix

--- upload_prod_tagged.sh
+++ upload_prod_tagged.sh
@@ -6,7 +6,6 @@ echo "Search for Docker-Hub-Password for OBEU in Jira."
 
 upload_images(){
     cd $PWD/docker-config && \
-        docker-compose -f prod_tagged.yml pull && \
         docker-compose -f prod_tagged.yml build && \
         docker-compose -f prod_tagged.yml push
 }
