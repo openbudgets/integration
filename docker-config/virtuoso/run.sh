d5764a832e269930b8b9f728650433f9043d52e4
mlukasch <maiklukasch@gmail.com>
2017-02-14 16:53:15 +0100

    fix

--- docker-config/virtuoso/run.sh
+++ docker-config/virtuoso/run.sh
@@ -2,6 +2,8 @@
 
 echo "export DBA_PASSWORD=$DBA_PASSWORD" > /setEnv
 
+cp /virtuoso.ini /data/virtuoso.ini
+
 nohup syslog-ng &
 nohup cron &
 nohup anacron &
--- docker-config/virtuoso/staging/startVirtuoso.sh
+++ docker-config/virtuoso/staging/startVirtuoso.sh
@@ -2,6 +2,6 @@
 # Start Virtuoso server:
 chmod a+x -R /etc/cron.*/
 #cp /virtuoso.ini /usr/local/virtuoso-opensource/var/lib/virtuoso/db/virtuoso.ini
-cp /virtuoso.ini /data/virtuoso.ini
 
-/bin/sh /virtuoso.sh
+
+/bin/bash /virtuoso.sh
