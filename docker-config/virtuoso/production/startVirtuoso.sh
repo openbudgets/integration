eb52de1066018906e0544a1ec13c83cd144c5f11
mlukasch <maiklukasch@gmail.com>
2017-02-14 15:43:29 +0100

    fix

--- docker-config/virtuoso/production/startVirtuoso.sh
+++ docker-config/virtuoso/production/startVirtuoso.sh
@@ -6,7 +6,8 @@ crontab mycron
 rm mycron
 # (crontab -l 2>/dev/null; echo "0 * * * * /cronjobs/syncTriplestores") | crontab -
 
-cp /virtuoso.ini /usr/local/virtuoso-opensource/var/lib/virtuoso/db/virtuoso.ini
+#cp /virtuoso.ini /usr/local/virtuoso-opensource/var/lib/virtuoso/db/virtuoso.ini
+cp /virtuoso.ini /data/virtuoso.ini
 
 # Start Virtuoso server:
 /bin/sh /virtuoso.sh
--- docker-config/virtuoso/staging/startVirtuoso.sh
+++ docker-config/virtuoso/staging/startVirtuoso.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Start Virtuoso server:
 chmod a+x -R /etc/cron.*/
-cp /virtuoso.ini /usr/local/virtuoso-opensource/var/lib/virtuoso/db/virtuoso.ini
+#cp /virtuoso.ini /usr/local/virtuoso-opensource/var/lib/virtuoso/db/virtuoso.ini
+cp /virtuoso.ini /data/virtuoso.ini
 
 /bin/sh /virtuoso.sh
