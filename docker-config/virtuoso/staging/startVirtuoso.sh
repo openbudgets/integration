a82354abd09ca91d04ef6fc74e7794aec2a05fa0
mlukasch <maiklukasch@gmail.com>
2017-02-14 18:47:13 +0100

    fix

--- docker-config/virtuoso/staging/startVirtuoso.sh
+++ docker-config/virtuoso/staging/startVirtuoso.sh
@@ -3,6 +3,8 @@
 # Start Virtuoso server:
 chmod a+x -R /etc/cron.*/
 #cp /virtuoso.ini /usr/local/virtuoso-opensource/var/lib/virtuoso/db/virtuoso.ini
-
+syslog-ng
+anacron
+cron
 
 /bin/bash /virtuoso.sh
