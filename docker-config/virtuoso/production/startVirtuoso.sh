f19a7c2ebcfba76991f8b57be622e4eacc9885aa
mlukasch <maiklukasch@gmail.com>
2017-02-14 17:55:39 +0100

    fix

--- docker-config/virtuoso/production/startVirtuoso.sh
+++ docker-config/virtuoso/production/startVirtuoso.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Delay restart in case of errors:
-sleep 2s
-
 chmod a+x -R /etc/cron.*/
 # Install cronjob:
 #(crontab -l 2>/dev/null; echo "* * * * * /cronjobs/syncTriplestores") | crontab -
