e4107644c1a3a4820e55339a5aa18319510c5339
mlukasch <maiklukasch@gmail.com>
2017-02-15 11:40:48 +0100

    fix

--- removeVirtuosoLocks.sh
+++ removeVirtuosoLocks.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd ./volumes && find . -name *.lck | xargs -i rm {}
-cd ./volumes && find . -name *.trx | xargs -i rm {}
+cd ./volumes && find . -name *.lck | xargs -i rm {} && \
+    find . -name *.trx | xargs -i rm {}
