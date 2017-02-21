04de07590ea50ba75531e764266121f92c136a10
mlukasch <maiklukasch@gmail.com>
2017-02-21 17:09:05 +0100

    fix

--- docker-config/virtuoso/cronjobs/installCronjobs.sh
+++ docker-config/virtuoso/cronjobs/installCronjobs.sh
@@ -2,6 +2,6 @@
 
 importFromTrig="0 */12 * * * /cronjobs/importFromTrig"
 importFromTTL="15 */12 * * * /cronjobs/importFromTTL"
-(crontab -u userhere -l; echo "$importFromTrig\n$importFromTTL" ) | crontab -u root -
+(crontab -u root -l; echo "$importFromTrig\n$importFromTTL" ) | crontab -u root -
 
 
