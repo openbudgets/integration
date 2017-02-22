d686a5990f87840529081faf12a7b05b6400d85d
mlukasch <maiklukasch@gmail.com>
2017-02-22 17:18:46 +0100

    added letsEncrypt for microsite subdomain

--- letsencrypt/letsEncryptAddCronjob.sh
+++ letsencrypt/letsEncryptAddCronjob.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-# Execute this script first in order to install a cronjob for renewing the TLS-certificate
-# Needs to be executed from the folder where the script is placed in order to work correctly
-crontab -l > mycron
-# echo new cron into cron file
-CMD="cd $PWD && sh ./letsEncryptRenewCert.sh"
-echo "@weekly $CMD" >> mycron
-# install new cron file
-crontab mycron
-rm mycron
+
+chmod +x ./*
+
+rsync -a ./letsEncryptRenewCert /etc/cron.daily/.
