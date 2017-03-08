df0c52b3b57369ad901105c5afb62042700edecd
mlukasch <maiklukasch@gmail.com>
2017-03-08 16:27:06 +0100

    fix https for alignment

--- docker-config/alignmentui/AppServiceProvider.php
+++ docker-config/alignmentui/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         if(env('APP_ENV') !== 'local')
         {
