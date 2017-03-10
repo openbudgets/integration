b18ab18a49b7052930c49091ca79ef417a295830
mlukasch <maiklukasch@gmail.com>
2017-03-10 18:27:53 +0100

    fix rudolf

--- docker-config/rudolf/start.sh
+++ docker-config/rudolf/start.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
 set -e
 
-set_file_permissions(){
-    chmod -R 777 /var/www/rudolf/storage
-    chown -R www-data:www-data /var/www/rudolf
-}
-
 startRudolf() {
     echo "Starting Rudolf"
     cd /var/www/rudolf
-    set_file_permissions
     php-fpm -R
 }
 
 refreshCache() {
     echo "Refreshing Cache in Rudolf..."
-    set_file_permissions
     cd /
     nohup /bin/sh /refreshCache >>/dev/null 2>>/dev/null &
-    set_file_permissions
 }
 
 ## Execute:
