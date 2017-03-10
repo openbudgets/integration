7fe4dca32a920051edd597964e0b5d46cbb200a1
mlukasch <maiklukasch@gmail.com>
2017-03-10 17:49:05 +0100

    fix rudolf

--- docker-config/rudolf/start.sh
+++ docker-config/rudolf/start.sh
@@ -10,8 +10,12 @@ startRudolf() {
 
 refreshCache() {
     echo "Refreshing Cache in Rudolf..."
+    chmod a+rwx -R /var/www/rudolf
+    chown -R www-data:www-data /var/www/rudolf
     cd /
     nohup /bin/sh /refreshCache >>/dev/null 2>>/dev/null &
+    chmod a+rwx -R /var/www/rudolf
+    chown -R www-data:www-data /var/www/rudolf
 }
 
 ## Execute:
