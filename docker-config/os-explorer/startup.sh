db403cf54cfccccf9da5d5f13aa1cc26f17e7405
mlukasch <maiklukasch@gmail.com>
2017-03-14 10:31:20 +0100

    revert os explorer

--- docker-config/os-explorer/startup.sh
+++ docker-config/os-explorer/startup.sh
@@ -1,21 +1,14 @@
 #!/bin/sh
 set -e
 
-ls $WORKDIR/.git > /dev/null && cd $WORKDIR || cd /app
-echo working from `pwd`
+cd /app
+node_modules/.bin/gulp
 
-if [ ! -z "$GIT_REPO" ]; then
-    rm -rf /remote || true && git clone $GIT_REPO /remote && cd /remote;
-    if [ ! -z "$GIT_BRANCH" ]; then
-        git checkout origin/$GIT_BRANCH
-    fi
-    cd /remote && npm install && npm run build
-else
-    ( cd /repos/os-explorer && npm install && npm run build  ) || true
-fi
+rm /www || true
+ln -s `pwd` /www
+chmod a+rwx /www
+ls -la /www/
 
-echo "{\"baseUrl\":\"\", \"snippets\": {\"ga\": \"$OS_SNIPPETS_GA\"}}" > config.json
+echo '{"baseUrl":""}' > /www/config.json
 
-ls -la
-find public
-node server.js
+nginx
