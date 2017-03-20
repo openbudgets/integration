f85f28bbdadd987d8b111d399b0f0a9078f41e13
mlukasch <maiklukasch@gmail.com>
2017-03-20 15:42:55 +0100

    update os-explorer

--- docker-config/os-explorer/config.js
+++ docker-config/os-explorer/config.js
@@ -1,5 +1,6 @@
 globalConfig = {
     baseUrl: 'http://eis-openbudgets.iais.fraunhofer.de/',
+    searchUrl: 'http://eis-openbudgets.iais.fraunhofer.de/search/package',
     snippets: {
         ga: null
     }
