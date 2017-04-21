7abaca5800adf59b2ea6c4cc7a4c02b12b71f238
Lazaros Ioannidis <larjohn@gmail.com>
2017-04-22 01:28:40 +0300

    Update sparql.php

--- docker-config/rudolf/config/sparql.php
+++ docker-config/rudolf/config/sparql.php
@@ -6,13 +6,23 @@
  * Time: 20:12:49
  */
 
+
 return [
     "prefixes"=>[
         'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
         'qb' => 'http://purl.org/linked-data/cube#',
         'skos'=>'http://www.w3.org/2004/02/skos/core#',
         'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-        'xro' => 'http://purl.org/xro/ns#'
+        'xro' => 'http://purl.org/xro/ns#',
     ],
-    "endpoint" =>'http://virtuoso_staging:8890/sparql'
+    "excusedPrefixes" => [
+        'bif' => 'http://www.openlinksw.com/schemas/bif#'
+    ],
+    "endpoint" =>'http://virtuoso_staging:8890/sparql',
+    "virtuoso" => true,
+    "languagesOrder" => [
+       "en", "de", "fr", "el", "es", "cs"
+    ],
+    "defaultAuthor" => "OpenBudgets.eu <info@openbudgets.eu>"
+
 ];
