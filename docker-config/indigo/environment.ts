b93243875285b4c3297a25b371a6ee358af7cc38
Lazaros Ioannidis <larjohn@gmail.com>
2017-06-08 00:33:30 +0300

    Update environment.ts

--- docker-config/indigo/environment.ts
+++ docker-config/indigo/environment.ts
@@ -10,5 +10,10 @@ export const environment = {
   DAMUrl: 'http://damapp:5000/',
   openCpuEndpoint: 'http://okfnrg.math.auth.gr/ocpu/',
   versionSuffix: '3',
-  baseHref: '/indigo/'
+  baseHref: '/indigo/',
+  DAMUrl: 'http://dam-obeu.iais.fraunhofer.de',
+  DAMretries: 10,
+  DAMpollingInitialStep: 1000,
+ searchSize: 50,
+
 };
