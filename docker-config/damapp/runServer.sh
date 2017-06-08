fcae36b6da26c7ac1f3d4f5fdf2d917755d1ab10
liyakun <liyakun127@gmail.com>
2017-06-08 12:44:50 +0200

    provide sample data endpoint for damapp

--- docker-config/damapp/runServer.sh
+++ docker-config/damapp/runServer.sh
@@ -3,6 +3,8 @@ cd $APP_DIR && \
     source ./env/bin/activate && \
     source ./.env
 
+mkdir ./sample-data && cd ./sample-data
+wget http://wenxion.net/OBEU/aggregate.json && cd ../
 nohup redis-server > /var/log/redis.log 2>&1 &
 nohup python3 worker.py > /var/log/worker.log 2>&1 &
 python3 manage.py runserver --host 0.0.0.0
