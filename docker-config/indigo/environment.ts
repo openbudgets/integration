650ba7492446d0270e452c082f0f6d4e42a63a8e
Lazaros Ioannidis <larjohn@gmail.com>
2017-08-02 12:32:09 +0300

    Update environment.ts

--- docker-config/indigo/environment.ts
+++ docker-config/indigo/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   //apiUrl: 'http://eis-openbudgets.iais.fraunhofer.de',
   apiUrl: 'http://apps.openbudgets.eu',
   //DAMUrl: "http://okfnrg.math.auth.gr/ocpu/",
