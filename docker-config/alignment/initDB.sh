e33d0ccbd17f1b366e64bb785ddbc8bf7262a390
skarampatakis <s.karampatakis@gmail.com>
2017-09-15 16:30:19 +0300

    fixing alignment not ready

--- docker-config/alignment/initDB.sh
+++ docker-config/alignment/initDB.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-yes | php artisan migrate --force
-nohup php artisan db:seed --force > /dev/null 2>&1 &
-nohup php artisan queue:listen --timeout=600 --sleep=30 > /dev/null 2>&1 &
+yes | php artisan migrate --seed --force
+nohup php artisan queue:listen --timeout=600 --sleep=30 --tries=5 > /dev/null 2>&1 &
--- docker-config/alignment/start.sh
+++ docker-config/alignment/start.sh
@@ -18,7 +18,8 @@ sh $APP_DIR/initDB.sh
 
 # Configure Apache Document Root
 mkdir -p $APACHE_DOC_ROOT
-chown www-data:www-data $APACHE_DOC_ROOT
+chown -R www-data:www-data $APACHE_DOC_ROOT
+chown -R www-data:www-data $APP_DIR/storage
 sed -i "s|DocumentRoot /var/www/html\$|DocumentRoot $APACHE_DOC_ROOT|" /etc/apache2/sites-available/000-default.conf
 echo "<Directory $APACHE_DOC_ROOT>" > /etc/apache2/conf-available/document-root-directory.conf
 echo "	AllowOverride All" >> /etc/apache2/conf-available/document-root-directory.conf
