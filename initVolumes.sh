165523661e5612af894b083d0667ec04b7dbfeec
skarampatakis <s.karampatakis@gmail.com>
2017-09-18 16:40:35 +0300

    added log volume for alignment

--- initVolumes.sh
+++ initVolumes.sh
@@ -71,6 +71,7 @@ mkdir -p volumes/alignment/composer
 mkdir -p volumes/alignment/storage
 mkdir -p volumes/alignment/storage/app/projects
 mkdir -p volumes/alignment/storage/app/public/json_serializer
+mkdir -p volumes/alignment/storage/logs
 mkdir -p volumes/alignment/stappler
 
 chmod -R oga+rwx ./volumes
