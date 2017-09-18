37fa0d8deb25afe0fc09fead26a2b4ba127bcbfe
skarampatakis <s.karampatakis@gmail.com>
2017-09-18 17:25:47 +0300

    changed listener user

--- docker-config/alignment/initDB.sh
+++ docker-config/alignment/initDB.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 yes | php artisan migrate --seed --force
-nohup php artisan queue:listen --timeout=600 --sleep=10 --tries=5 > storage/logs/worker.log &
+su - www-data -c "nohup php artisan queue:listen --timeout=600 --sleep=10 --tries=5 > storage/logs/worker.log &"
