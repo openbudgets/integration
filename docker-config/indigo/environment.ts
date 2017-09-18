d5dd803867701d2817fa74f49246f39fcb20e25c
Yakun Li <liyakun127@gmail.com>
2017-09-18 11:47:38 +0200

    change opencpu endpoint to fhg of indigo and damapp

--- docker-config/indigo/environment.ts
+++ docker-config/indigo/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: true,
   apiUrl: 'http://apps.openbudgets.eu',
   DAMUrl: 'http://apps.openbudgets.eu/dam',
-  openCpuEndpoint: 'http://localhost:8666/ocpu',
+  openCpuEndpoint: 'http://apps.openbudgets.eu/ocpu',
   versionSuffix: '3',
   baseHref: '/',
   DAMretries: 10,
