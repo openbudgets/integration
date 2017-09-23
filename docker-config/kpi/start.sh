a338ad876b7dd545a5d32e463035687e5d35a4d0
sotos <s.karampatakis@gmail.com>
2017-09-23 22:03:22 +0300

    fixed KPI login

--- docker-config/kpi/start.sh
+++ docker-config/kpi/start.sh
@@ -14,7 +14,7 @@ chown www-data:www-data /var/www/database/database.sqlite
 # Configure Apache Document Root
 mkdir -p $APACHE_DOC_ROOT
 chown www-data:www-data $APACHE_DOC_ROOT
-sed -i "s|DocumentRoot /var/www/html\$|DocumentRoot $APACHE_DOC_ROOT|" /etc/apache2/sites-available/000-default.conf
+#sed -i "s|DocumentRoot /var/www/html\$|DocumentRoot $APACHE_DOC_ROOT|" /etc/apache2/sites-available/000-default.conf
 echo "<Directory $APACHE_DOC_ROOT>" > /etc/apache2/conf-available/document-root-directory.conf
 echo "	AllowOverride All" >> /etc/apache2/conf-available/document-root-directory.conf
 echo "	Require all granted" >> /etc/apache2/conf-available/document-root-directory.conf
