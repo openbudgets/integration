6863013b6e48a13f615f539d9e74fb33a1e952cb
Yakun Li <liyakun127@gmail.com>
2017-09-29 17:30:54 +0200

    clean docker stack

--- docker-config/obeu-microsite/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd /microsite
-
-rsync --ignore-existing
-
-python manage.py runserver 0.0.0.0:8000
--- docker-config/obeu-microsite/superuser.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd /microsite
-wait 5
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('$SUPERUSER_MAIL', '$SUPERUSER_NAME', '$SUPERUSER_PW')" | python manage.py shell | tail -f
