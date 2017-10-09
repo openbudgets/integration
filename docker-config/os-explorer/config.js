b236a438a3a309fc33a2905649c877a612613991
Yakun Li <liyakun127@gmail.com>
2017-10-09 11:13:23 +0200

    remove non used components

--- docker-config/os-explorer/config.js
+++
@@ -1,9 +0,0 @@
-globalConfig = {
-    //baseUrl: 'http://eis-openbudgets.iais.fraunhofer.de/',
-    baseUrl: 'http://apps.openbudgets.eu/',
-    //searchUrl: 'http://eis-openbudgets.iais.fraunhofer.de/search/package',
-    searchUrl: 'http://apps.openbudgets.eu/search/package',
-    snippets: {
-        ga: null
-    }
-};
