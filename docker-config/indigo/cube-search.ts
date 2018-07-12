80b732d35feebf986feb2672deb22c1f544aa15b
liyakun <liyakun127@gmail.com>
2018-07-12 18:13:05 +0200

    fix translation

---
+++ docker-config/indigo/cube-search.ts
@@ -0,0 +1,59 @@
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/distinctUntilChanged';
+import {Component, Output, Input, EventEmitter, AfterViewInit} from '@angular/core';
+
+
+@Component({
+  selector: 'indigo-cube-search',
+  template: `
+    <md-card>
+      <md-card-title>Daten finden</md-card-title>
+      <md-card-content>
+        <md-input-container>
+          <input mdInput placeholder="Suche" [value]="query" (keyup)="search.emit($event.target.value)"/>
+        </md-input-container>
+        <md-spinner *ngIf="searching" [class.show]="searching"></md-spinner>
+      </md-card-content>
+    </md-card>`,
+  styles: [`
+    md-card-title,
+    md-card-content {
+      display: flex;
+      justify-content: center;
+    }
+
+    md-input {
+      display: flex;
+      justify-content: center;
+    }
+
+    md-card-spinner {
+      padding-left: 60px; // Make room for the spinner
+    }
+
+    md-spinner {
+      width: 30px;
+      height: 30px;
+      position: relative;
+      top: 10px;
+      left: 10px;
+      opacity: 0.0;
+    }
+
+    md-spinner.show {
+      opacity: 1.0;
+    }
+  `]
+})
+export class CubeSearchComponent implements AfterViewInit {
+  @Input() query = '';
+  @Input() searching = false;
+  @Output() search = new EventEmitter<string>();
+  ngAfterViewInit(): void {
+    this.search.emit('');
+
+  }
+
+
+}
